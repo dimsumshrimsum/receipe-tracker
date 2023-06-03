6982412fa4947658117f372f82a46dafc34f23ff
Zach Robles <zrobles1188@gmail.com>
2023-06-03 15:24:32 -0400

    only did the one commit lol

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React, { useState } from "react";
 import "./App.css";
 import RecipeCreate from "./RecipeCreate";
 import RecipeList from "./RecipeList";
-import RecipeData from "./RecipeData"
+import RecipeData from "./RecipeData";
 
 function App() {
   const [recipes, setRecipes] = useState(RecipeData);
 
   // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
   // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+  function handleFormSubmit(newRecipe) {
+    setRecipes([...recipes, newRecipe]);
+  }
+  function deleteRecipe(recipeToDelete) {
+    let filterRecipes = recipes.filter((recipe) => {
+      return recipe !== recipeToDelete;
+    });
+    setRecipes(filterRecipes);
+  }
 
-  
   return (
     <div className="App">
-      <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList />
-      <RecipeCreate />
+      <header>
+        <h1>Delicious Food Recipes</h1>
+      </header>
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe} />
+      <RecipeCreate handleFormSubmit={handleFormSubmit} />
     </div>
   );
 }
